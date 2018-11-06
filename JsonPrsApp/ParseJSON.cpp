20068d3eb478d4b9f45c5e81b2049ac84e6bb6b0
高萩 俊行 <tosiyuki@hh.iij4u.or.jp>
2018-11-06 17:40:54 +0900

    パーサー本体の構築ができたのでひとまずチェックイン。 ダイアログそのものがテストコードになっている。

--- JsonPrsApp/ParseJSON.cpp
+++ JsonPrsApp/ParseJSON.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "ParseJSON.h"
 
+#include <string>
+
 namespace Morrin::JSON
 {
 inline  bool IsWhiteSpace( std::string_view::value_type value )
@@ -25,14 +27,17 @@ inline std::string_view::size_type SkipWhiteSpace( const std::string_view& rowDa
 	return offset;
 }
 static bool ParseElement( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
-static bool ParseElements( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
+static bool ParseMember( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
+static bool ParseValues( std::function<bool( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )>&& parseProc,
+						 ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
 
 static bool ParseObject( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
 static bool ParseArray( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
-static bool ParseString( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset, NodeType type );
-static bool ParseLiteralValue( ParseCallBack& proc, std::string_view& rowData, std::string_view::size_type& offset, const std::string_view& checkValue, NodeType type );
-
+static bool ParseStringValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
+static bool ParseLiteralValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset, const std::string_view& checkValue, NotificationId type );
 static bool ParseNumberValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset );
+
+static std::string_view ExtractStringValue( const std::string_view& rowData, std::string_view::size_type& offset );
 
 //	エレメントのパース
 static bool ParseElement( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
@@ -58,25 +63,25 @@ static bool ParseElement( ParseCallBack& proc, const std::string_view& rowData, 
 			break;
 		case '"':	//	string
 			//	オブジェクトの途中ではないので、オブジェクトキーではない
-			if( !ParseString( proc, rowData, offset, NodeType::String ) )
+			if( !ParseStringValue( proc, rowData, offset ) )
 			{
 				return false;
 			}
 			break;
 		case 't':	//	リテラルチェック == true
-			if( !ParseLiteralValue( proc, rowData, offset, "true", NodeType::True ) )
+			if( !ParseLiteralValue( proc, rowData, offset, "true", NotificationId::True ) )
 			{
 				return false;
 			}
 			break;
 		case 'f':	//	リテラルチェック == false
-			if( !ParseLiteralValue( proc, rowData, offset, "false", NodeType::False ) )
+			if( !ParseLiteralValue( proc, rowData, offset, "false", NotificationId::False ) )
 			{
 				return false;
 			}
 			break;
 		case 'n':	//	リテラルチェック == null
-			if( !ParseLiteralValue( proc, rowData, offset, "null", NodeType::Null ) )
+			if( !ParseLiteralValue( proc, rowData, offset, "null", NotificationId::Null ) )
 			{
 				return false;
 			}
@@ -93,65 +98,170 @@ static bool ParseElement( ParseCallBack& proc, const std::string_view& rowData, 
 	}
 	return true;
 }
-static bool ParseElements( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
+static bool ParseMember( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
+{
+	offset = SkipWhiteSpace( rowData, offset );	//	'{' の次から空白部分を除外
+	_ASSERTE( rowData[offset] == '"' );
+	auto key = ExtractStringValue( rowData, offset );
+	if( !proc( NotificationId::Key, key ) )
+	{
+		return false;
+	}
+	offset = SkipWhiteSpace( rowData, offset );
+	_ASSERTE( rowData[offset] == ':' );
+	//	データがおかしい場合はそこで終了でいい
+	if( rowData[offset++] != ':' )
+	{
+		return false;
+	}
+	if( !ParseElement( proc, rowData, offset ) )
+	{
+		return false;
+	}
+	offset = SkipWhiteSpace( rowData, offset );
+	return true;
+}
+static bool ParseValues( std::function<bool( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )>&& parseProc,
+						 ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
 {
 	while( offset < rowData.length() )
 	{
-		if( !ParseElement( proc, rowData, offset ) )
+		if( !parseProc( proc, rowData, offset ) )
 		{
 			return false;
 		}
-		//	Elements は ',' が後続する場合、もう一回 Elementが来る
+		//	Elements/Members は ',' が後続する場合、もう一回 Element/Member が来る
 		if( offset < rowData.length() && rowData[offset] == ',' )
 		{
-			++offset;	//	','は後続で参照されない
+			//	区切りを通知する
+			if( !proc( NotificationId::Comma, rowData.substr( offset++, 1 ) ) )
+			{
+				return false;	//	キャンセルされた
+			}
 		}
 		else
 		{
 			break;
 		}
 	}
-	//	処理を正常に終了したので成功終了する
+	//	処理を正常に終了したので成功終了する	//	Elements の終わりは何も言わない
 	return true;
 }
 static bool ParseObject( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
 {
 	_ASSERTE( rowData[offset] == '{' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
-	return false;
+	if( !proc( NotificationId::StartObject, rowData.substr( offset++, 1 ) ) )
+	{
+		return false;
+	}
+	if( !ParseValues( ParseMember, proc, rowData, offset ) )
+	{
+		return false;
+	}
+	_ASSERTE( rowData[offset] == '}' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
+	if( rowData[offset] != '}' )
+	{
+		return false;
+	}
+	return proc( NotificationId::EndObject, rowData.substr( offset++, 1 ) );
 }
 static bool ParseArray( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
 {
 	_ASSERTE( rowData[offset] == '[' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
-	return false;
+	if( !proc( NotificationId::StartArray, rowData.substr( offset++, 1 ) ) )
+	{
+		return false;
+	}
+	if( !ParseValues( ParseElement, proc, rowData, offset ) )
+	{
+		return false;
+	}
+	offset = SkipWhiteSpace( rowData, offset );
+	_ASSERTE( rowData[offset] == ']' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
+	if( rowData[offset] != ']' )
+	{
+		return false;
+	}
+	return proc( NotificationId::EndArray, rowData.substr( offset++, 1 ) );
 }
-static bool ParseString( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset, NodeType type )
+static bool ParseStringValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
 {
 	_ASSERTE( rowData[offset] == '"' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
-	return false;
+	auto value = ExtractStringValue( rowData, offset );	//	末尾の '"' の次まで移動。実際の値は参照しないけど、ここで切り出しているので長さは見れる
+	return proc( NotificationId::String, value );
 }
-static bool ParseLiteralValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset, const std::string_view& checkValue, NodeType type )
+static bool ParseLiteralValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset, const std::string_view& checkValue, NotificationId type )
 {
-	bool result = false;
 	if( rowData.compare( offset, checkValue.length(), checkValue ) == 0 )
 	{
-		result = proc( type, rowData, offset, checkValue.length() );	//	有効なのは、データサイズだけなのでその分をセットする
-		offset += checkValue.length();	//	Literalデータサイズ分をスキップ
+		offset += checkValue.length();
+		return proc( type, checkValue );
 	}
-	return result;
+	return false;
 }
 static bool ParseNumberValue( ParseCallBack& proc, const std::string_view& rowData, std::string_view::size_type& offset )
 {
 	//	数値データは、数字もしくは、- で始まる。
 	_ASSERTE( isdigit( rowData[offset] ) || rowData[offset] == '-' );	//	数値または、- で始まる
-	return false;
+	//	整数ばかりとは限らないので、数値データは文字列のまま渡す。理想は、int/double 当たりなんだろうけど。。。
+	auto start = rowData.data()+offset;
+	char* end = nullptr;
+	auto doubleValue = strtod( start, &end );
+	auto length = end-start;
+	auto value = rowData.substr( offset, length );
+	offset += length;
+	return proc( NotificationId::Digit, value );
+}
+static std::string_view ExtractStringValue( const std::string_view& rowData, std::string_view::size_type& offset )
+{
+	_ASSERTE( rowData[offset] == '"' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
+	++offset;	//	'"' をスキップ
+	auto start = offset;
+	//	テキストの終端部分までステップする
+	while( offset < rowData.length() && rowData[offset] != '"' )
+	{
+		//	エスケープ記号
+		if( rowData[offset++] == '\\' )
+		{
+			switch( rowData[offset++] )
+			{
+			case 'u':
+				//	規定で4文字だけど念のため。厳格チェックしない場合は+4でスキップしてよい
+				for( int cnt = 0 ; cnt < 4 ; ++cnt, ++offset )
+				{
+					//	本当は途中で終わるとか言語道断レベルよね。。。
+					if( !isxdigit( rowData[offset] ) )
+					{
+						return false;
+					}
+				}
+				break;
+			//	厳格チェックしない場合は、このあたりはコメントアウトでよい
+			case '"':
+			case '\\':
+			case '/':
+			case 'b':
+			case 'n':
+			case 'r':
+			case 't':
+				break;
+			default:
+				return false;
+			}
+		}
+	}
+	_ASSERTE( rowData[offset] == '"' );	//	手前でスキップしないので、ここで再度値チェックできるようにしておく
+	auto result = rowData.substr( start, offset-start );
+	++offset;
+	return result;
 }
 ///////////////////////////////////////////////////////////////////////////////
 //	global Functions.
 bool APIENTRY ParseJSON( const std::string_view& rowData, ParseCallBack& proc )
 {
 	std::string_view::size_type offset = 0;
-	//	開始を通知
-	if( !proc( NodeType::StartParse, rowData, offset, std::string_view::npos ) )
+	//	開始を通知(空文字列)
+	if( !proc( NotificationId::StartParse, rowData ) )
 	{
 		return false;
 	}
@@ -170,6 +280,75 @@ bool APIENTRY ParseJSON( const std::string_view& rowData, ParseCallBack& proc )
 		}
 	}
 	//	終了を通知。offset の位置で判定可能なので必要があればそれを見ることとする
-	return proc( NodeType::EndParse, rowData, offset, std::string_view::npos );
+	return proc( NotificationId::EndParse, rowData.substr( offset ) );	//	なにか残ってるという想定で全部送り込んでやる(さらにネスト的な処理があってもよいという判定)
 }
+//	テキストのエスケープ処理を解決する
+std::string APIENTRY ExtractEscapeString( const std::string_view& escapeValue )
+{
+	std::string_view::size_type pos = 0;
+	std::string resultU8Str;
+	auto prevPos = pos;
+	while( pos < escapeValue.length() )
+	{
+		//	エスケープ記号
+		if( escapeValue[pos] == '\\' )
+		{
+			if( prevPos != std::string_view::npos )
+			{
+				resultU8Str += escapeValue.substr( prevPos, pos-prevPos );
+			}
+			prevPos = std::string_view::npos;	//	エスケープ文字内部なのでリセット
+			++pos;	//	無視
+			switch( escapeValue[pos] )
+			{
+			case 'u':
+				//	規定で4文字だけど念のため。厳格チェックしない場合は+4でスキップしてよい
+				{
+					wchar_t ch = 0;
+					for( int cnt = 0 ; cnt < 4 && isxdigit( escapeValue[pos] ) ; ++cnt, ++pos )
+					{
+						if( isdigit( escapeValue[pos] ) )
+						{
+							ch = (ch << 4) + (escapeValue[pos] - '0');
+						}
+						else
+						{
+							ch = (ch<<4) + ( (toupper( escapeValue[pos] ) - 'A') + 0xA);
+						}
+					}
+					char utf8str[4+1];	//	UTF8は最大4文字に分解される
+					WideCharToMultiByte( CP_UTF8, 0, &ch, 1, utf8str, 4+1, nullptr, nullptr );
+					resultU8Str += utf8str;
+				}
+				break;
+			//	厳格チェックしない場合は、このあたりはコメントアウトでよい
+			case '"':
+			case '\\':
+			case '/':
+			case 'b':
+			case 'n':
+			case 'r':
+			case 't':
+				resultU8Str += escapeValue[pos];
+				++pos;
+				break;
+			default:
+				return false;
+			}
+		}
+		else
+		{
+			if( prevPos == std::string_view::npos )
+			{
+				prevPos = pos;
+			}
+			++pos;
+		}
+	}
+	if( prevPos != std::string_view::npos )
+	{
+		resultU8Str += escapeValue.substr( prevPos, pos-prevPos );
+	}
+	return resultU8Str;
 }
+}//	namespace Morrin::JSON
--- JsonPrsApp/ParseJSON.h
+++ JsonPrsApp/ParseJSON.h
@@ -3,7 +3,7 @@
 //	コンセプト
 //	目指せ最速！だからコピーしない！
 //	できるだけ型は既定のものを使う！
-//	JSONデータの書式の規定は http://www.json.org/json-ja.html を参照した(2018/11/05時点)
+//	JSONデータの書式の規定は http://www.json.org/json-ja.html を参照(2018/11/05時点)
 
 //	VC++以外ではビルドしてない！
 
@@ -15,7 +15,7 @@
 
 namespace Morrin::JSON
 {
-enum NodeType
+enum NotificationId
 {
 	StartParse,		//	パース開始(これからデータが送られてくる)
 	EndParse,		//	パース終了(これ以上データが送られてくることはない)
@@ -24,6 +24,8 @@ enum NodeType
 	EndObject,		//	'}'
 	StartArray,		//	'['
 	EndArray,		//	']'
+	//	配列要素のセパレータ
+	Comma,
 	//	単一型の通知
 	Key,			//	オブジェクトのキー(実際は文字列)
 	String,			//	文字列データ
@@ -34,8 +36,12 @@ enum NodeType
 };
 
 //	パース結果のコールバック受け取りメソッド( bool func( Morrin::JSON::NodeType nodeType, const std::string_view& node ) )
-using ParseCallBack = std::function<bool( NodeType nodeType, const std::string_view& rowData, std::string_view::size_type off, std::string_view::size_type count )> const;
+using ParseCallBack = std::function<bool( NotificationId id, const std::string_view& value )> const;
 
 //	JSONパースエンジン本体
 bool APIENTRY ParseJSON( const std::string_view& rowData, ParseCallBack& proc );
+std::string APIENTRY ExtractEscapeString( const std::string_view& escapeValue );
+
+//	JSONデータの汎用型クラスを作る？として。。。pair<key,value> valueを std::any にするか、variant<int,double,u8string,vector<int,double,u8string>>にするか。。。かなぁ？
+
 }//	Morin::JSON
